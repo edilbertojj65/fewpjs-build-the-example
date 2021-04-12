61a546f3dae0ea5be0c0b2988c3ba4c1c30c692a
edilbertojj65 <edilbertojj65@gmail.com>
2021-04-11 21:30:07 -0400

    Completed assigment

--- main.js
+++ main.js
@@ -3,10 +3,45 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
+
+const colorStates = {
+  "red" : "",
+  "": "red"
+};
+
+const classLike = document.querySelectorAll(".like-glyph");
+
+ function glyphCallback(ev){
+    const heart = ev.target;
+    mimicServerCall("Url")
+   //OR: mimicServerCall("bogusUrl", {forceFailure: true})
+    .then(function(serverMessage){
+       heart.innerText = glyphStates[heart.innerText];
+       heart.style.color = colorStates[heart.style.color];
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+
+ }
 
 
+    for (var x = 0 ; x < (classLike.length - 1); x++ ) {
+      
+      classLike[x].addEventListener("click", glyphCallback);
+ 
+    }
+        
 
 
+  
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
